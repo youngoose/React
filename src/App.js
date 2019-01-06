b649783851c3020d72f14bd44937d89989b1924e
youngoose <edwardjo811@gmail.com>
2019-01-06 11:39:06 -0500

    practice class component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Greet />
+        <Welcome />
+      </div>
+    );
+  }
+}
+
+export default App;
